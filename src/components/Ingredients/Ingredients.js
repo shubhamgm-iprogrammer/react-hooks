d2cf4d7c75567701090c73bd6eac7dce6d5d5797
shubhamgm.iprogrammer <shubhamgm.iprogrammer@gmail.com>
2021-04-29 19:06:25 +0530

    practicing-react-hooks

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,54 @@
+import React, { useState ,useEffect} from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+
+  useEffect(() => {
+    fetch('https://hooks-74d13-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+  const addIngredientHandler = ingredient => {
+
+    fetch("https://hooks-74d13-default-rtdb.firebaseio.com/ingredients.json",{
+      method:"POST",
+      body:JSON.stringify(ingredient),
+      headers:{'Content-Type':'application/json'}
+    }).then(response => {
+       return response.json()
+    }).then(responseData => {
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients,
+      { id: responseData.name, ...ingredient }
+    ]);
+  })
+}
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
